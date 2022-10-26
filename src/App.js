f3954ff5286ebfb22a7046028d1cd580706b35b3
panglima_burung <dozen.khaya@gmail.com>
2022-10-27 01:46:30 +0700

    upd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Home from './components/Home/Home';
+import MovieDetail from './components/MovieDetails/MovieDetail';
+
+import Login from './containers/Login';
+import Register from './containers/Register';
+import LandingPage from './containers/LandingPage';
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+          <div className='container'>  
+            <Routes>
+              <Route exact path='/' element={<LandingPage/>}/>
+              <Route exact path='/Login' element={<ProtectedRoute ><Login/></ProtectedRoute>}/>
+              <Route exact path='Register' element={<ProtectedRoute><Register/></ProtectedRoute>}/>
+              <Route exact path='/Home' element={<ProtectedRoute loginOnly={true}><Home/></ProtectedRoute>}/>
+              <Route exact path='/movie/:imdbID' element={<ProtectedRoute loginOnly={true}><MovieDetail/></ProtectedRoute>}/>
+            </Routes>
+          </div>
+        </Router>
     </div>
   );
 }
